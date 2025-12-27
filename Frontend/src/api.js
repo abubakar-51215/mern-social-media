a39aa7a20036f2d0cec8dce812b2544d39919962
Abubakar Amir <abubakeramir95@gmail.com>
2025-12-27 21:14:19 +0500

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './utils/auth';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: (window.ENV && window.ENV.REACT_APP_API_URL) || process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 // Add token to requests
