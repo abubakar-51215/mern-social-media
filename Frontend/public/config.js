7b24af1588d9e43595efa6ec07369a4e574ae894
Abubakar Amir <abubakeramir95@gmail.com>
2025-12-27 21:12:54 +0500

    Create config.js

---
+++ Frontend/public/config.js
@@ -0,0 +1,5 @@
+// Runtime configuration - can be changed without rebuilding
+window.ENV = {
+  REACT_APP_API_URL: 'https://api.talhasghar.site/api',
+  REACT_APP_SOCKET_URL: 'https://api.talhasghar.site'
+};
